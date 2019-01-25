fe3e2ba1df42bea1da915bbd84abd52ce7bc435a
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2019-01-25 07:25:34 +0100

    question 3.4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'], 
+  animations: [
+    trigger('goals',[
+      transition('* => *',[
+       query(':enter', style({opacity: 0}), {optional: true}),
+       query(':enter', stagger('300ms',[
+         animate('.6s ease-in', keyframes([
+           style({opacity: 0, transform: 'translateY(-75%)', offset :0}),
+           style({opacity: .5, transform: 'translateY(35px)', offset :.3}),
+           style({opacity: 1, transform: 'translateY(0)', offset :1}),
+          ]))]),{optional: true}) 
+       ])
+      ])
+    ]
 })
 export class HomeComponent implements OnInit {
 
@@ -16,7 +31,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.nbItems = this.objectifs.length;
   }
-  
+
   ajoutItem()
   {
     this.objectifs.push(this.objectifText);
