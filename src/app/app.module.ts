c15c1a0521708f0bb1e275fa9a720c8911c52aae
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2019-01-25 07:06:44 +0100

    question 3.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     FormsModule,
     BrowserModule,
     AppRoutingModule
