892d4941965325615e082af69134711f5e0ac744
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2019-01-25 09:45:48 +0100

    partie 3 anim OK

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,14 +7,14 @@ import { trigger, style, transition, animate, keyframes, query, stagger} from '@
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'], 
   animations: [
-    trigger('goals',[
+    trigger('objectifs',[
       transition('* => *',[
        query(':enter', style({opacity: 0}), {optional: true}),
        query(':enter', stagger('300ms',[
          animate('.6s ease-in', keyframes([
-           style({opacity: 0, transform: 'translateY(-75%)', offset :0}),
-           style({opacity: .5, transform: 'translateY(35px)', offset :.3}),
-           style({opacity: 1, transform: 'translateY(0)', offset :1}),
+           style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+           style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+           style({opacity: 1, transform: 'translateY(0)', offset: 1}),
           ]))]),{optional: true}) 
        ])
       ])
@@ -25,7 +25,7 @@ export class HomeComponent implements OnInit {
   nbItems: number;
   btnText: string  = "Ajouter un item";
   objectifText : string = "Mon nouvel objectif";
-  objectifs = [];
+  objectifs = ["Un element", "deux elements", "trois elements"];
 
   constructor() { }
   ngOnInit() {
