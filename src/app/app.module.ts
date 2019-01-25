9f7a33964310d11f7c520108ca298108ee530bb7
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2019-01-25 06:15:39 +0100

    question 2.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,6 +13,7 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
