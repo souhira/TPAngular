2468930ce3bd2b65fe7a673900938450ac941250
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2019-01-28 00:30:53 +0100

    liste d'items dans About

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActiviteService } from '../services/activite.service';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activiteService: ActiviteService) { 
+
+    
+  }
 
   ngOnInit() {
   }
